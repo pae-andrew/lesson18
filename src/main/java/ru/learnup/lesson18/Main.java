19f7ec40fc432b8a8d2277b5c58e31b000c55dd7
andrew <vip.paevskiy@gmail.com>
2022-03-16 00:15:29 +0300

    Добавил экземпляр класса Calculator, чтобы запись была локаничней и понятней

--- src/main/java/ru/learnup/lesson18/Main.java
+++ src/main/java/ru/learnup/lesson18/Main.java
@@ -14,12 +14,13 @@ public class Main {
         LOG.debug("Start our program...");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+        Calculator calculator = context.getBean(Calculator.class);
 
         System.out.println();
-        System.out.printf("Результат сложения 3 и 6: %d\n", context.getBean(Calculator.class).sum(3, 6));
-        System.out.printf("Резултат вычитания 6 из 13: %d\n", context.getBean(Calculator.class).subtract(13, 6));
-        System.out.printf("Результат деления 21 на 7: %d\n", context.getBean(Calculator.class).divide(21, 7));
-        System.out.printf("Результат умножения 5 на 3: %d\n", context.getBean(Calculator.class).multiply(5, 3));
+        System.out.printf("Результат сложения 3 и 6: %d\n", calculator.sum(3, 6));
+        System.out.printf("Резултат вычитания 6 из 13: %d\n", calculator.subtract(13, 6));
+        System.out.printf("Результат деления 21 на 7: %d\n", calculator.divide(21, 7));
+        System.out.printf("Результат умножения 5 на 3: %d\n", calculator.multiply(5, 3));
         System.out.println();
 
         LOG.debug("Program ended successfully.");
